cfd98bd1f5788fa9ddbbce4a35950452146ddf4f
Vicky Aliaga <garrascris@gmail.com>
2019-05-07 18:32:12 -0400

    Primer commit

---
+++ Persona.java
@@ -0,0 +1,18 @@
+public class Persona{
+	private  int id;
+	private String nombre;
+	private String telefono;
+
+	public Persona()
+	{
+
+	}
+	public int getId()
+	{
+		return this.id;
+	}
+	public void setId(int id)
+	{
+		this.id=id;
+	}
+}
